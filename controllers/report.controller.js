bbc8e55b0341ae435de9ee7ac67e3efeae3bfb16
Muhammad Syaifullah Nurrahman <syaifullah.nurrahman@outlook.com>
2026-02-11 21:18:39 +0700

    [update] update get category

--- controllers/report.controller.js
+++ controllers/report.controller.js
@@ -1,72 +1,17 @@
 import db from '../models/index.js'; // Pastikan path ini sesuai dengan loader Anda
-import { Op } from 'sequelize';
 import ResponseBuilder from "../utils/response.js";
-import { getPagination, getPagingData } from "../utils/pagination.js";
+import { getPagingData } from "../utils/pagination.js";
+import * as reportService from '../services/report.service.js'
 
 const { AssetView, Sequelize } = db;
 
 export const getAssetReport = async (req, res, next) => {
-    const {
-        page = 1,
-        size = 10,
-        search,
-        category,
-        subCategory,
-        condition,
-        yearBuilt,
-        asset_type,
-        ...otherFilters
-    } = req.query;
     const responseBuilder = new ResponseBuilder(res)
-
-    const { limit, offset } = getPagination(page, size);
+    const page = req.query.page ? parseInt(req.query.page) : null;
+    const size = req.query.size ? parseInt(req.query.size) : null;
 
     try {
-        let whereClause = {};
-
-        if (search) {
-            whereClause[Op.or] = [
-                { name: { [Op.iLike]: `%${search}%` } },
-                { reg_number: { [Op.iLike]: `%${search}%` } }
-            ];
-        }
-
-        if (condition) whereClause.condition = condition;
-        if (yearBuilt) whereClause.yearBuilt = yearBuilt;
-        if (asset_type) whereClause.asset_type = asset_type;
-        if (category) whereClause.category = category;
-        if (subCategory) whereClause.subCategory = subCategory;
-
-        Object.keys(otherFilters).forEach(key => {
-            if (key.startsWith('prop_')) {
-                const jsonKey = key.replace('prop_', ''); // ambil key asli, misal 'vendor'
-                const value = otherFilters[key];
-
-                whereClause[Op.and] = [
-                    ...(whereClause[Op.and] || []), // Pertahankan existing AND jika ada
-                    Sequelize.where(
-                        Sequelize.literal(`"properties"->>'${jsonKey}'`),
-                        value
-                    )
-                ];
-            }
-        });
-
-        const { count, rows } = await AssetView.findAndCountAll({
-            where: whereClause,
-            attributes: [
-                'id', 'layerId', 'regNumber', 'name', 'layerName', 'category', 'subCategory', 'condition', 'yearBuilt', 'managedBy', 'properties', 'assetType'
-            ],
-            limit,
-            offset,
-            order: [['created_at', 'DESC']]
-        });
-
-        const data = {
-            count,
-            rows,
-            isPaginated: true,
-        }
+        const data = await reportService.getAssetReport(req.query);
 
         let payload = getPagingData(data, page, size)
 
@@ -87,27 +32,83 @@ export const getCategoryReport = async (req, res, next) => {
     const data = [
         {
             name: "Bangungan Gedung",
-            value: "bangunan-gedung"
+            value: "bangunan-gedung",
+            subCategory: [
+                {
+                    name: "Bangungan Gedung",
+                    value: "bangunan-gedung",
+                }
+            ]
         },
         {
             name: "Jaringan Jalan dan Jembatan",
-            value: "jaringan-jalan-dan-jembatan"
+            value: "jaringan-jalan-dan-jembatan",
+            subCategory: [
+                {
+                    name: "Drainase Perkotaan",
+                    value: "drainase-perkotaan"
+                },
+                {
+                    name: "Pengendalian Banjir",
+                    value: "pengendalian-banjir"
+                }
+            ]
         },
         {
             name: "Drainase Perkotaan dan Pengendalian Banjir",
-            value: "drainase-perkotaan-dan-pengendalian-banjir"
+            value: "drainase-perkotaan-dan-pengendalian-banjir",
+            subCategory: [
+                {
+                    name: "Drainase Perkotaan",
+                    value: "drainase-perkotaan"
+                },
+                {
+                    name: "Pengendalian Banjir",
+                    value: "pengendalian-banjir"
+                }
+            ]
         },
         {
             name: "Bangunan Sumber Daya Air dan Irigasi",
-            value: "bangunan-sumber-daya-air-dan-irigasi"
+            value: "bangunan-sumber-daya-air-dan-irigasi",
+            subCategory: [
+                {
+                    name: "Bangunan Sumber Daya Air",
+                    value: "bangunan-sumber-daya-air"
+                },
+                {
+                    name: "Irigasi",
+                    value: "irigasi"
+                }
+            ]
         },
         {
             name: "Jaringan Air Minum",
-            value: "jaringan-air-minum"
+            value: "jaringan-air-minum",
+            subCategory: [
+                {
+                    name: "Jaringan Air Minum",
+                    value: "jaringan-air-minum"
+                }
+            ]
         },
         {
             name: "Pengolahaan Air Limbah dan Limbah B3 dan Sanitasi",
-            value: "pengolahaan-air-limbah-dan-limbah-b3-dan-sanitasi"
+            value: "pengolahaan-air-limbah-dan-limbah-b3-dan-sanitasi",
+            subCategory: [
+                {
+                    name: "Pengendalian Air Limbah",
+                    value: "pengendalian-air-limbah"
+                },
+                {
+                    name: "Pengendalian Limbah B3",
+                    value: "limbah-b3-dan-limbah"
+                },
+                {
+                    name: "Sanitasi",
+                    value: "sanitasi"
+                }
+            ]
         }
     ]
 
